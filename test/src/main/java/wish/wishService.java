bbfbfec9d95ee8be54f04a129557883a8f9f8b27
hennie-yun <hennie9401@gmail.com>
2023-04-28 10:23:01 +0900

    wish table vo, dao, service

---
+++ test/src/main/java/wish/wishService.java
@@ -0,0 +1,30 @@
+package wish;
+
+import java.util.ArrayList;
+
+public class wishService {
+
+	private wishDao dao;
+	
+	public wishService() {
+		dao = new wishDao(); 
+	}
+
+	//찜 추가
+	public void addwish(wishVo vo) {
+		dao.insert(vo);
+	}
+	
+	//찜 삭제
+	public void delwish(String userId) {
+		dao.delwish(userId);
+	}
+	
+	//찜 리스트 뽑기 
+	public ArrayList<wishVo> getWishList(String userId) {
+		return dao.WishList(userId);
+	}
+
+
+}
+
